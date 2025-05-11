005e7d73f2918ee20a5dd12ec13d235bc628292e
roioha3 <roioha@post.bgu.ac.il>
2025-05-11 15:51:19 +0000

    implemented tournament

--- user/libtournament.c
+++ user/libtournament.c
@@ -3,9 +3,13 @@
 #include "user/user.h"
 
 int * locks;
+int * pids;
+int processesNum;
 
 int tournament_create(int processes){
+    processesNum = processes;
     locks = malloc(sizeof(int*) * (processes-1));
+    pids = malloc(sizeof(int*) * processes);
     for (int i = 0; i < processes - 1; i++) {
         locks[i] = peterson_create();
         if (locks[i] == -1)
@@ -18,14 +22,57 @@ int tournament_create(int processes){
             return -1;
         
         
-        if (n == 0) // if child
+        if (n == 0) { // if child
+            pids[i] = getpid();
             return i; // tournament ID
+        }
     }
+    pids[processes-1] = getpid();
     return processes-1;
 
 
 }
 
-int tournament_acquire(void);
+int tournament_acquire(void){
+    int index = getTournamentID(getpid());
 
-int tournament_release(void);
+    int levels = log(processesNum);
+
+    for (int l = levels - 1; l >= 0; l--) {
+        int role =  (index & (1 << (levels - l - 1))) >> (levels - l - 1);
+        int lock = index >> levels - l;
+
+        peterson_acquire(locks[lock], role);
+    }
+
+}
+
+int getTournamentID(int pid) {
+    for (int i = 0; i < processesNum; i++){
+        if (pid == pids[i])
+            return i;
+    }
+    return -1;
+}
+
+int log(int n) {
+    int count = 0;
+    while(n != 1) {
+        count++;
+        n = n << 1;
+    }
+    return count;
+}
+
+
+int tournament_release(void) {
+    int index = getTournamentID(getpid());
+    int levels = log(processesNum);
+
+    for (int l = 0; l < levels; l++) {
+        int role =  (index & (1 << (levels - l - 1))) >> (levels - l - 1);
+        int lock = index >> levels - l;
+
+        peterson_release(locks[lock], role);
+    }
+}
---
+++ user/tournament.c
@@ -0,0 +1,13 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char** argv) {
+    if (argc == 0) {
+        printf("incorrect usage! torunament <number of proccesses>\n");
+        exit(-1);
+    }
+
+    
+
+}
